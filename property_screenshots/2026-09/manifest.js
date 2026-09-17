2b753ea3431b00cb6a9ec9d27d8e5dde21d55adc
conging <annsyuu38@gmail.com>
2026-09-17 18:29:07 +0900

    Improve readable property screenshots

--- property_screenshots/2026-09/manifest.js
+++ property_screenshots/2026-09/manifest.js
@@ -1,151 +1,127 @@
 window.MONTHLY_SCREENSHOTS = {
   "LA・MU": [
-    "property_screenshots/2026-09/lamu_01_9-1.png"
+    "property_screenshots/2026-09/lamu_01_9-01.png"
   ],
   "あみやき亭": [
-    "property_screenshots/2026-09/amiyaki_01_sheet-1.png"
+    "property_screenshots/2026-09/amiyaki_01_sheet-01.png"
   ],
   "いがっこ給食センター": [
-    "property_screenshots/2026-09/school_lunch_01_9-1.png"
+    "property_screenshots/2026-09/school_lunch_01_9-01.png"
   ],
   "すき家 伊賀上野店": [
-    "property_screenshots/2026-09/sukiya_01_Sheet1-1.png",
-    "property_screenshots/2026-09/sukiya_01_Sheet1-2.png",
-    "property_screenshots/2026-09/sukiya_01_Sheet1-3.png",
-    "property_screenshots/2026-09/sukiya_01_Sheet1-4.png",
-    "property_screenshots/2026-09/sukiya_01_Sheet1-5.png",
-    "property_screenshots/2026-09/sukiya_01_Sheet1-6.png",
-    "property_screenshots/2026-09/sukiya_01_Sheet1-7.png"
+    "property_screenshots/2026-09/sukiya_01_Sheet1-01.png"
   ],
   "ほっともっと 伊賀桑町店": [
-    "property_screenshots/2026-09/hotto_01_sheet-1.png"
+    "property_screenshots/2026-09/hotto_01_sheet-01.png"
   ],
   "ほっともっと 伊賀白鳳通り店": [
-    "property_screenshots/2026-09/hotto_02_sheet-1.png"
+    "property_screenshots/2026-09/hotto_02_sheet-01.png"
   ],
   "イオンタウン伊賀": [
-    "property_screenshots/2026-09/aeon_01_sheet-1.png"
+    "property_screenshots/2026-09/aeon_01_sheet-01.png"
   ],
   "びっくりドンキー 伊賀上野店": [
-    "property_screenshots/2026-09/aeon_02_sheet-1.png"
+    "property_screenshots/2026-09/aeon_02_sheet-01.png"
   ],
   "KFC イオンタウン伊賀上野店": [
-    "property_screenshots/2026-09/aeon_03_KFC-1.png"
+    "property_screenshots/2026-09/aeon_03_KFC-01.png"
   ],
   "クスリのアオキ 伊賀上野店": [
-    "property_screenshots/2026-09/drug_aoki_ueno_01_sheet-1.png"
+    "property_screenshots/2026-09/drug_aoki_ueno_01_sheet-01.png"
   ],
   "クスリのアオキ 平野東店": [
-    "property_screenshots/2026-09/drug_aoki_hirano_01_sheet-1.png"
+    "property_screenshots/2026-09/drug_aoki_hirano_01_sheet-01.png"
   ],
   "コカ・コーラボトラーズ": [
-    "property_screenshots/2026-09/coca_cola_01_9-1.png"
+    "property_screenshots/2026-09/coca_cola_01_9-01.png"
   ],
   "コメリ ＨＣ上野店": [
-    "property_screenshots/2026-09/komeri_01_sheet-1.png",
-    "property_screenshots/2026-09/komeri_01_sheet-2.png"
+    "property_screenshots/2026-09/komeri_01_sheet-01.png"
   ],
   "セブンイレブン 伊賀緑ヶ丘東町店（店舗コード:1420）": [
-    "property_screenshots/2026-09/seven_01_sheet-1.png"
+    "property_screenshots/2026-09/seven_01_sheet-01.png"
   ],
   "セブンイレブン 伊賀東条店（店舗コード:1442）": [
-    "property_screenshots/2026-09/seven_01_sheet-1.png"
+    "property_screenshots/2026-09/seven_01_sheet-01.png"
   ],
   "セブンイレブン 伊賀平野北谷店（店舗コード:1431）": [
-    "property_screenshots/2026-09/seven_01_sheet-1.png"
+    "property_screenshots/2026-09/seven_01_sheet-01.png"
   ],
   "セブンイレブン 伊賀四十九町北店（店舗コード:1432）": [
-    "property_screenshots/2026-09/seven_01_sheet-1.png"
+    "property_screenshots/2026-09/seven_01_sheet-01.png"
   ],
   "セブンイレブン 伊賀緑ヶ丘本町店（店舗コード:1422）": [
-    "property_screenshots/2026-09/seven_01_sheet-1.png"
+    "property_screenshots/2026-09/seven_01_sheet-01.png"
   ],
   "セブンイレブン 伊賀平野東町店（店舗コード:1449）": [
-    "property_screenshots/2026-09/seven_01_sheet-1.png"
+    "property_screenshots/2026-09/seven_01_sheet-01.png"
   ],
   "セブンイレブン 伊賀忍者市駅南店（店舗コード:2837）": [
-    "property_screenshots/2026-09/seven_01_sheet-1.png"
+    "property_screenshots/2026-09/seven_01_sheet-01.png"
   ],
   "セカンドストリート 伊賀小田店（店舗コード:3628）": [
-    "property_screenshots/2026-09/seven_01_sheet-1.png"
+    "property_screenshots/2026-09/seven_01_sheet-01.png"
   ],
   "チョーヤ梅酒": [
-    "property_screenshots/2026-09/choya_01_Sheet1-1.png",
-    "property_screenshots/2026-09/choya_01_Sheet1-2.png"
+    "property_screenshots/2026-09/choya_01_Sheet1-01.png"
   ],
   "ナフコ 伊賀店": [
-    "property_screenshots/2026-09/nafco_01_Sheet1-1.png",
-    "property_screenshots/2026-09/nafco_01_Sheet1-2.png",
-    "property_screenshots/2026-09/nafco_01_Sheet1-3.png",
-    "property_screenshots/2026-09/nafco_01_Sheet1-4.png"
+    "property_screenshots/2026-09/nafco_01_Sheet1-01.png"
   ],
   "マックスバリュ 上野店": [
-    "property_screenshots/2026-09/max_ueno_01_Sheet1-1.png"
+    "property_screenshots/2026-09/max_ueno_01_Sheet1-01.png"
   ],
   "マックスバリュ 佐那具店": [
-    "property_screenshots/2026-09/max_sanagu_01_Sheet1-1.png"
+    "property_screenshots/2026-09/max_sanagu_01_Sheet1-01.png"
   ],
   "㈱やさいの戸島屋": [
-    "property_screenshots/2026-09/tojimaya_01_Sheet1-1.png",
-    "property_screenshots/2026-09/tojimaya_01_Sheet1-2.png"
+    "property_screenshots/2026-09/tojimaya_01_Sheet1-01.png"
   ],
   "三重県伊賀庁舎": [
-    "property_screenshots/2026-09/iga_government_01_Sheet1-1.png",
-    "property_screenshots/2026-09/iga_government_01_Sheet1-2.png"
+    "property_screenshots/2026-09/iga_government_01_Sheet1-01.png"
   ],
   "上野法務合同庁舎": [
-    "property_screenshots/2026-09/ueno_legal_01_8_9-1.png"
+    "property_screenshots/2026-09/ueno_legal_01_8_9-01.png"
   ],
   "農業屋 伊賀上野店": [
-    "property_screenshots/2026-09/farm_shop_01_Sheet1-1.png",
-    "property_screenshots/2026-09/farm_shop_01_Sheet1-2.png"
+    "property_screenshots/2026-09/farm_shop_01_Sheet1-01.png"
   ],
   "魚民 上野市駅前店": [
-    "property_screenshots/2026-09/uomasa_01_Sheet1-1.png",
-    "property_screenshots/2026-09/uomasa_01_Sheet1-2.png",
-    "property_screenshots/2026-09/uomasa_02_Sheet2-1.png"
+    "property_screenshots/2026-09/uomasa_01_Sheet1-01.png",
+    "property_screenshots/2026-09/uomasa_02_Sheet2-01.png"
   ],
   "古本市場 上野店": [
-    "property_screenshots/2026-09/furuhon_01_Sheet1-1.png",
-    "property_screenshots/2026-09/furuhon_01_Sheet1-2.png"
+    "property_screenshots/2026-09/furuhon_01_Sheet1-01.png"
   ],
   "検察庁": [
-    "property_screenshots/2026-09/prosecutors_01_sheet-1.png"
+    "property_screenshots/2026-09/prosecutors_01_sheet-01.png"
   ],
   "津地方裁判所": [
-    "property_screenshots/2026-09/district_court_01_sheet-1.png",
-    "property_screenshots/2026-09/district_court_01_sheet-2.png",
-    "property_screenshots/2026-09/district_court_02_sheet-1.png"
+    "property_screenshots/2026-09/district_court_01_sheet-01.png",
+    "property_screenshots/2026-09/district_court_02_sheet-01.png"
   ],
   "西松屋 伊賀上野店": [
-    "property_screenshots/2026-09/nishimatsuya_01_sheet-1.png",
-    "property_screenshots/2026-09/nishimatsuya_01_sheet-2.png",
-    "property_screenshots/2026-09/nishimatsuya_01_sheet-3.png"
+    "property_screenshots/2026-09/nishimatsuya_01_sheet-01.png"
   ],
   "厚板プレス工業": [
-    "property_screenshots/2026-09/atsuita_01_Sheet1-1.png",
-    "property_screenshots/2026-09/atsuita_01_Sheet1-2.png"
+    "property_screenshots/2026-09/atsuita_01_Sheet1-01.png"
   ],
   "旧上野庁舎 SAKAKURA BASE": [
-    "property_screenshots/2026-09/sakakura_01_9-1.png"
+    "property_screenshots/2026-09/sakakura_01_9-01.png"
   ],
   "伊賀の街": [
-    "property_screenshots/2026-09/i_gamachi_01_9-1.png",
-    "property_screenshots/2026-09/i_gamachi_01_9-2.png"
+    "property_screenshots/2026-09/i_gamachi_01_9-01.png"
   ],
   "ライフテクノサービス": [
-    "property_screenshots/2026-09/i_gamachi_01_9-1.png",
-    "property_screenshots/2026-09/i_gamachi_01_9-2.png"
+    "property_screenshots/2026-09/i_gamachi_01_9-01.png"
   ],
   "伊賀神戸駅": [
-    "property_screenshots/2026-09/iga_kobe_01_Sheet1-1.png",
-    "property_screenshots/2026-09/iga_kobe_01_Sheet1-2.png"
+    "property_screenshots/2026-09/iga_kobe_01_Sheet1-01.png"
   ],
   "創価学会 伊賀上野文化会館": [
-    "property_screenshots/2026-09/soka_01_4-1.png",
-    "property_screenshots/2026-09/soka_01_4-2.png"
+    "property_screenshots/2026-09/soka_01_4-01.png"
   ],
   "飯田金属": [
-    "property_screenshots/2026-09/iida_01_22-1.png"
+    "property_screenshots/2026-09/iida_01_22-01.png"
   ]
 };
