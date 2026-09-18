1a09295c6501f556cec954744ae506f15fc35c15
conging <annsyuu38@gmail.com>
2026-09-18 18:54:53 +0900

    Add named property screenshots to memo

--- property_screenshots/2026-09/manifest.js
+++ property_screenshots/2026-09/manifest.js
@@ -78,36 +78,41 @@ window.MONTHLY_SCREENSHOTS = {
     "property_screenshots/2026-09/tojimaya_01_Sheet1-01.png"
   ],
   "三重県伊賀庁舎": [
-    "property_screenshots/2026-09/iga_government_01_Sheet1-01.png"
+    "property_screenshots/2026-09/iga_government_20260918.png"
   ],
   "上野法務合同庁舎": [
-    "property_screenshots/2026-09/ueno_legal_01_8_9-01.png"
+    "property_screenshots/2026-09/ueno_legal_20260918.png"
   ],
   "農業屋 伊賀上野店": [
-    "property_screenshots/2026-09/farm_shop_01_Sheet1-01.png"
+    "property_screenshots/2026-09/agri_20260918.png"
+  ],
+  "ドラッグセイムス 伊賀小田店": [
+    "property_screenshots/2026-09/drug_seims_20260918.png"
+  ],
+  "ドラッグセイムス 伊賀上野店": [
+    "property_screenshots/2026-09/drug_seims_20260918.png"
   ],
   "魚民 上野市駅前店": [
-    "property_screenshots/2026-09/uomasa_01_Sheet1-01.png",
-    "property_screenshots/2026-09/uomasa_02_Sheet2-01.png"
+    "property_screenshots/2026-09/uomasa_sheet1_20260918.png",
+    "property_screenshots/2026-09/uomasa_sheet2_20260918.png"
   ],
   "古本市場 上野店": [
     "property_screenshots/2026-09/furuhon_01_Sheet1-01.png"
   ],
-  "検察庁": [
-    "property_screenshots/2026-09/prosecutors_01_sheet-01.png"
-  ],
   "津地方裁判所": [
-    "property_screenshots/2026-09/district_court_01_sheet-01.png",
-    "property_screenshots/2026-09/district_court_02_sheet-01.png"
+    "property_screenshots/2026-09/district_court_20260918.png"
   ],
   "西松屋 伊賀上野店": [
-    "property_screenshots/2026-09/nishimatsuya_01_sheet-01.png"
+    "property_screenshots/2026-09/nishimatsuya_20260918.png"
   ],
   "厚板プレス工業": [
-    "property_screenshots/2026-09/atsuita_01_Sheet1-01.png"
+    "property_screenshots/2026-09/atsuita_20260918.png"
   ],
   "旧上野庁舎 SAKAKURA BASE": [
-    "property_screenshots/2026-09/sakakura_01_9-01.png"
+    "property_screenshots/2026-09/sakakura_20260918.png"
+  ],
+  "検察庁": [
+    "property_screenshots/2026-09/prosecutors_20260918.png"
   ],
   "伊賀の街": [
     "property_screenshots/2026-09/i_gamachi_01_9-01.png"
