c210f8ac9739d40bfebd5feba444e9ab9b9e07f3
conging <annsyuu38@gmail.com>
2026-09-18 20:34:10 +0900

    Register complete September property screenshots

--- property_screenshots/2026-09/manifest.js
+++ property_screenshots/2026-09/manifest.js
@@ -3,79 +3,79 @@ window.MONTHLY_SCREENSHOTS = {
     "property_screenshots/2026-09/lamu_01_9-01.png"
   ],
   "あみやき亭": [
-    "property_screenshots/2026-09/amiyaki_01_sheet-01.png"
+    "property_screenshots/2026-09/amiyaki_20260918.png"
   ],
   "いがっこ給食センター": [
-    "property_screenshots/2026-09/school_lunch_01_9-01.png"
+    "property_screenshots/2026-09/school_lunch_20260918.png"
   ],
   "すき家 伊賀上野店": [
-    "property_screenshots/2026-09/sukiya_01_Sheet1-01.png"
+    "property_screenshots/2026-09/sukiya_20260918.png"
   ],
   "ほっともっと 伊賀桑町店": [
-    "property_screenshots/2026-09/hotto_01_sheet-01.png"
+    "property_screenshots/2026-09/hotto_01_20260918.png"
   ],
   "ほっともっと 伊賀白鳳通り店": [
-    "property_screenshots/2026-09/hotto_02_sheet-01.png"
+    "property_screenshots/2026-09/hotto_02_20260918.png"
   ],
   "イオンタウン伊賀": [
-    "property_screenshots/2026-09/aeon_01_sheet-01.png"
+    "property_screenshots/2026-09/aeon_20260918.png"
   ],
   "びっくりドンキー 伊賀上野店": [
-    "property_screenshots/2026-09/aeon_02_sheet-01.png"
+    "property_screenshots/2026-09/donki_20260918.png"
   ],
   "KFC イオンタウン伊賀上野店": [
-    "property_screenshots/2026-09/aeon_03_KFC-01.png"
+    "property_screenshots/2026-09/kfc_20260918.png"
   ],
   "クスリのアオキ 伊賀上野店": [
-    "property_screenshots/2026-09/drug_aoki_ueno_01_sheet-01.png"
+    "property_screenshots/2026-09/drug_aoki_ueno_20260918.png"
   ],
   "クスリのアオキ 平野東店": [
-    "property_screenshots/2026-09/drug_aoki_hirano_01_sheet-01.png"
+    "property_screenshots/2026-09/drug_aoki_hirano_20260918.png"
   ],
   "コカ・コーラボトラーズ": [
-    "property_screenshots/2026-09/coca_cola_01_9-01.png"
+    "property_screenshots/2026-09/coca_cola_20260918.png"
   ],
   "コメリ ＨＣ上野店": [
-    "property_screenshots/2026-09/komeri_01_sheet-01.png"
+    "property_screenshots/2026-09/komeri_20260918.png"
   ],
   "セブンイレブン 伊賀緑ヶ丘東町店（店舗コード:1420）": [
-    "property_screenshots/2026-09/seven_01_sheet-01.png"
+    "property_screenshots/2026-09/seven_20260918.png"
   ],
   "セブンイレブン 伊賀東条店（店舗コード:1442）": [
-    "property_screenshots/2026-09/seven_01_sheet-01.png"
+    "property_screenshots/2026-09/seven_20260918.png"
   ],
   "セブンイレブン 伊賀平野北谷店（店舗コード:1431）": [
-    "property_screenshots/2026-09/seven_01_sheet-01.png"
+    "property_screenshots/2026-09/seven_20260918.png"
   ],
   "セブンイレブン 伊賀四十九町北店（店舗コード:1432）": [
-    "property_screenshots/2026-09/seven_01_sheet-01.png"
+    "property_screenshots/2026-09/seven_20260918.png"
   ],
   "セブンイレブン 伊賀緑ヶ丘本町店（店舗コード:1422）": [
-    "property_screenshots/2026-09/seven_01_sheet-01.png"
+    "property_screenshots/2026-09/seven_20260918.png"
   ],
   "セブンイレブン 伊賀平野東町店（店舗コード:1449）": [
-    "property_screenshots/2026-09/seven_01_sheet-01.png"
+    "property_screenshots/2026-09/seven_20260918.png"
   ],
   "セブンイレブン 伊賀忍者市駅南店（店舗コード:2837）": [
-    "property_screenshots/2026-09/seven_01_sheet-01.png"
+    "property_screenshots/2026-09/seven_20260918.png"
   ],
   "セカンドストリート 伊賀小田店（店舗コード:3628）": [
-    "property_screenshots/2026-09/seven_01_sheet-01.png"
+    "property_screenshots/2026-09/seven_20260918.png"
   ],
   "チョーヤ梅酒": [
-    "property_screenshots/2026-09/choya_01_Sheet1-01.png"
+    "property_screenshots/2026-09/choya_20260918.png"
   ],
   "ナフコ 伊賀店": [
-    "property_screenshots/2026-09/nafco_01_Sheet1-01.png"
+    "property_screenshots/2026-09/nafco_20260918.png"
   ],
   "マックスバリュ 上野店": [
-    "property_screenshots/2026-09/max_ueno_01_Sheet1-01.png"
+    "property_screenshots/2026-09/max_ueno_20260918.png"
   ],
   "マックスバリュ 佐那具店": [
-    "property_screenshots/2026-09/max_sanagu_01_Sheet1-01.png"
+    "property_screenshots/2026-09/max_sanagu_20260918b.png"
   ],
   "㈱やさいの戸島屋": [
-    "property_screenshots/2026-09/tojimaya_01_Sheet1-01.png"
+    "property_screenshots/2026-09/tojimaya_20260918.png"
   ],
   "三重県伊賀庁舎": [
     "property_screenshots/2026-09/iga_government_20260918.png"
@@ -84,7 +84,7 @@ window.MONTHLY_SCREENSHOTS = {
     "property_screenshots/2026-09/ueno_legal_20260918.png"
   ],
   "農業屋 伊賀上野店": [
-    "property_screenshots/2026-09/agri_20260918.png"
+    "property_screenshots/2026-09/agri_20260918b.png"
   ],
   "ドラッグセイムス 伊賀小田店": [
     "property_screenshots/2026-09/drug_seims_20260918.png"
@@ -97,10 +97,10 @@ window.MONTHLY_SCREENSHOTS = {
     "property_screenshots/2026-09/uomasa_sheet2_20260918.png"
   ],
   "古本市場 上野店": [
-    "property_screenshots/2026-09/furuhon_01_Sheet1-01.png"
+    "property_screenshots/2026-09/furuhon_20260918.png"
   ],
-  "津地方裁判所": [
-    "property_screenshots/2026-09/district_court_20260918.png"
+  "津地方裁判所伊賀支部": [
+    "property_screenshots/2026-09/district_court_20260918b.png"
   ],
   "西松屋 伊賀上野店": [
     "property_screenshots/2026-09/nishimatsuya_20260918.png"
@@ -115,18 +115,18 @@ window.MONTHLY_SCREENSHOTS = {
     "property_screenshots/2026-09/prosecutors_20260918.png"
   ],
   "伊賀の街": [
-    "property_screenshots/2026-09/i_gamachi_01_9-01.png"
+    "property_screenshots/2026-09/iga_machi_20260918.png"
   ],
   "ライフテクノサービス": [
-    "property_screenshots/2026-09/i_gamachi_01_9-01.png"
+    "property_screenshots/2026-09/life_techno_20260918.png"
   ],
   "伊賀神戸駅": [
-    "property_screenshots/2026-09/iga_kobe_01_Sheet1-01.png"
+    "property_screenshots/2026-09/iga_kobe_20260918b.png"
   ],
   "創価学会 伊賀上野文化会館": [
     "property_screenshots/2026-09/soka_01_4-01.png"
   ],
   "飯田金属": [
-    "property_screenshots/2026-09/iida_01_22-01.png"
+    "property_screenshots/2026-09/iida_20260918b.png"
   ]
 };
